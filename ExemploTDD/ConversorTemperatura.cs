e8cb1badb549c43897ded1522fb9058c228d4d66
Pablo Juan <pablojuan@foryousystem.com>
2016-07-19 23:22:01 -0300

    Teste MspCodingatNight

--- ExemploTDD/ConversorTemperatura.cs
+++ ExemploTDD/ConversorTemperatura.cs
@@ -4,9 +4,18 @@ namespace ExemploTDD
 {
     public static class ConversorTemperatura
     {
+        /// <summary>
+        /// Teste
+        /// </summary>
+        /// <param name="temperatura"></param>
+        /// <returns></returns>
         public static double FahrenheitParaCelsius(double temperatura)
         {
             return Math.Round((temperatura - 32) / 1.8, 2);
+
+
+
+
         }
     }
 }
